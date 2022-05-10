f2d3d6d79b39a77b85f4d6eb2d3ecb61e017c3a4
lechon1997 <odriozola-leo@hotmail.com>
2022-05-10 12:32:26 -0300

    navbar drop down

--- menu/menu-responsive/js/myjs.js
+++ menu/menu-responsive/js/myjs.js
@@ -1,9 +1,12 @@
-addEventListener('DOMContentLoaded',()=>{
-    const btn_menu = document.querySelector('.btn_menu')
-    if(btn_menu){
-        btn_menu.addEventListener('click', ()=>{
-            const menu_items = document.querySelector('.menu_items')
-            menu_items.classList.toggle('show')
-        })
-    }
-})
+addEventListener("DOMContentLoaded", () => {
+  const btn_menu = document.querySelector(".btn_menu");
+  if (btn_menu) {
+    btn_menu.addEventListener("click", () => {
+      const menu_items = document.querySelector(".menu");
+      const btn = document.getElementById("btn-icono");
+      menu_items.classList.toggle("show");
+      btn.classList.toggle("fa-times");
+      btn.classList.toggle("fa-bars");
+    });
+  }
+});
